88eef3a9c4a17e5a52a435d37982a2fada8ce5a7
sasha1107 <sasha1107@naver.com>
2024-11-03 19:46:57 +0900

    feat: font

---
+++ src/app/(local-font)/local-font/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-xl">Pretendard</h1>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. At iusto
+      recusandae sapiente voluptate eum cumque debitis dolor, quia nisi animi.
+      Molestiae amet, mollitia quos fugit hic dolorem quasi laudantium magni!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+      perspiciatis quae sint nobis nam quo nostrum minus explicabo molestias
+      repellendus itaque eius, id sunt debitis tenetur? Iure placeat esse
+      repellendus? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Sequi culpa distinctio voluptates itaque nostrum fuga aspernatur! Libero
+      exercitationem perspiciatis impedit, eius consequatur suscipit eveniet
+      eligendi voluptatum accusantium rerum aut consequuntur. Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. In consectetur, placeat itaque quos
+      aliquam quis dolorum repellendus ipsa enim atque ipsam iste assumenda
+      ullam? Ipsam excepturi in sed provident quos? Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Accusantium unde libero numquam tenetur
+      reprehenderit magni pariatur ipsa dignissimos necessitatibus nesciunt quae
+      impedit sed at explicabo exercitationem animi eum, officia sapiente! Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Hic corporis culpa,
+      beatae quo magni distinctio esse dolorem iusto at laudantium corrupti non
+      itaque cum iste, laboriosam impedit quam deserunt saepe?
+    </div>
+  );
+};
+
+export default Page;
